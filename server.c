d6829e132e1dcddc29a1121dd87989ed65fbd284
Brodie Schroeder <broschroeder@gmail.com>
2017-04-23 20:20:27 -0700

    Handle multiple clients. Add client file

--- server.c
+++ server.c
@@ -16,7 +16,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(char *msg) {
+void	error(char *msg)
+{
 	perror(msg);
 	exit(1);
+}
+
+int		main(int argc char *argv[])
+{
+	int	sockfd;
+	int	newsockfd;
+	int	portno;
+	int	clilen;
+	int	pid;
+	int	n;
+	char	buffer[256];
+	struct	sockaddr_in serv_addr;
+	struct	sockaddr_in cli_addr;
+	if (argc < 2)
+	{
+		fprintf(stderr, "Error, no port provided\n");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portno);
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+		sizeof(serv_addr)) < 0)
+			error("ERROR on binding");
+	listen(sockfd, 128);
+	clilen = sizeof(cli_addr);
+	while (1)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0)
+			error("ERROR on accept");
+		pid = fork();
+		if (pid < 0)
+			error("ERROR on fork");
+		if (pid == 0)
+		{
+			close(sockfd);
+			dostuff(newsockfd);
+			exit(0);
+		}
+		else
+			close(newsockfd);
+	}
+	return (0); /* Never gets here just for semantics */
+}
+
+void dostuff(int sock)
+{
+	int n;
+	char buffer[256];
+
+	bzero(buffer, 256);
+	n = read(sock, buffer, 255);
+	if (n < 0)
+		error("ERROR reading from socket");
+	printf("Here is the message: %s\n", buffer);
+	n = write(sock, "I got your message", 18);
+	if (n < 0)
+		error("ERROR writing to socket");
 }
